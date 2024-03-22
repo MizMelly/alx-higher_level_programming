9b6738753833f2fc5ee9821908770ebedb81a41a
MizMelly <jazminelike@yahoo.com>
2024-03-22 12:59:26 -0700

    2-print_alphabet.py

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+for i in range(97, 123):  # ASCII values for lowercase alphabet characters
+    print(chr(i), end='')  # Convert ASCII value to character and print without newline
