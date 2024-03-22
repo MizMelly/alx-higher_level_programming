7963ebcceeefd4454261212d98e8fa24b6cee044
MizMelly <jazminelike@yahoo.com>
2024-03-22 06:22:20 -0700

    8-concat_edges.py

---
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+str = "Python is an interpreted, interactive, object-oriented programming\
+ language that combines remarkable power with very clear syntax"
+print(str[65:68] + str[71:78] + str[91:95] + str[102:] + str[:34])
+print(str)
