fef2166263a14b790930903948f1259ee67ca4c2
MizMelly <jazminelike@yahoo.com>
2024-03-22 07:46:14 -0700

    10-check_cycle.c

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * check_cycle - function that checks if a list has a cycle
+ * @list: linked list
+ * return: 1 if there is cycle and 0 if not
+ */
+int check_cycle(listint_t *list)
+{
+listint_t *fast, *slow;
+
+if (!list || !list ->next)
+	return (0);
+fast = list;
+slow = list;
+
+while (slow != NULL && fast != NULL && fast->next != NULL)
+{
+	slow = slow->next;
+	fast = fast->next->next;
+	if (slow == fast)
+}
+}
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -5,20 +5,21 @@
 
 /**
  * struct listint_s - singly linked list
- * @n: interger
- * @next: it points to the next node
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: singly linked list structure node
- * for Holberton school project
+ * Description: singly linked list node structure
+ * 
  */
 typedef struct listint_s
 {
-	int n;
-}
-listint_t;
+    int n;
+    struct listint_s *next;
+} listint_t;
+
 size_t print_listint(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 void free_listint(listint_t *head);
 int check_cycle(listint_t *list);
 
-#endif
+#endif /* LISTS_H */
