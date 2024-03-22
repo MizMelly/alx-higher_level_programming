2b90705970cd2e8e0a6f196583c5d62dc3d6aea9
MizMelly <jazminelike@yahoo.com>
2024-03-22 08:07:42 -0700

    100-write.py

---
+++ 0x00-python-hello_world/100-write.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import sys
+
+def main():
+    message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
+    sys.stderr.write(message)
+    sys.exit(1)
+
+if __name__ == "__main__":
+    main()
