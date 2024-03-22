e5d1680a2688feef1b6a6af83c6763ed59ae4f6a
MizMelly <jazminelike@yahoo.com>
2024-03-22 08:12:56 -0700

    100-write.py

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-
-def main():
-    message = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
-    sys.stderr.write(message)
-    sys.exit(1)
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
