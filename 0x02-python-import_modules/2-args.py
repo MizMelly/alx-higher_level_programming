dbddd1284f03cd432a7c1ad9b4e89d5a646a5511
MizMelly <jazminelike@yahoo.com>
2024-04-10 07:10:13 -0700

    arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    x = len(argv) - 1
+    if x < 1:
+        print("{} arguments.".format(x))
+    elif x == 1:
+        print("{} argument:".format(x))
+    else:
+        print("{} arguments:".format(x))
+
+        for i in rang(x):
+            print("{}: {:s}".format(i + 1, argv[i + 1]))
