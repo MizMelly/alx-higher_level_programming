d2012142d053355eca8f512f1c880d4a29896c4c
MizMelly <jazminelike@yahoo.com>
2024-06-14 14:55:12 -0700

    comedy

---
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -0,0 +1,9 @@
+-- a script that lists all Comedy shows in the database hbtn_0d_tvshows
+SELECT t.`name`
+FROM `tv_shows` AS t
+INNER JOIN `tv_show_genres` AS s
+ON t.`id` = s.`show_id`
+INNER JOIN `tv_genres` AS g
+ON g.`id` = s.`genre_id`
+WHERE g.`name` = "Comedy"
+ORDER BY t.`title`;
