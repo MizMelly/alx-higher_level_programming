08080900ca196352cc607d285e5e95ff2af4a689
MizMelly <jazminelike@yahoo.com>
2024-06-14 14:49:13 -0700

    number of genre

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -4,8 +4,7 @@ SELECT g.`name`
 FROM `tv_genres` AS g
 INNER JOIN `tv_show_genres` AS s
 ON g.`id` = s.`genre_id`
-
 INNER JOIN `tv_shows` AS t
-ON t.`title` = s.`show_id`
+ON t.`id` = s.`show_id`
 WHERE t.`title` = "Dexter"
-ORDER BY g.`name` ;
+ORDER BY g.`name`;
