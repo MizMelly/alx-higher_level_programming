3d9b718aced03a1353561c1eb3f775daf0cf25df
MizMelly <jazminelike@yahoo.com>
2024-06-14 12:20:03 -0700

    creates tables with states

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,7 @@
 -- a script that creates the database hbtn_0d_usa and the table states
-CREATE DATABASE IF NOT EXISTS `hbtn_od_usa`;
-CREATE TABLE IF NOT EXISTS `hbtn_od_usa`.`states` (
-	PRIMARY KEY(`id`),
+CREATE DATABASE IF NOT EXISTS `hbtn_0d_usa`;
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`states` (
 	`id` INT NOT NULL AUTO_INCREMENT,
-	`name`  VARCHAR(256) NOT NULL
+	`name`  VARCHAR(256) NOT NULL,
+	 PRIMARY KEY(`id`)
 );
