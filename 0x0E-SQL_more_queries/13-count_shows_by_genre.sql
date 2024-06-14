54e3556ac677b80c95fb91a1520aaa37b3b453c7
MizMelly <jazminelike@yahoo.com>
2024-06-14 14:18:50 -0700

    number of genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -6,11 +6,11 @@
 -- Don’t display a genre that doesn’t have any shows linked
 -- Results must be sorted in descending order by the number of shows linked
 -- You can use only one SELECT statement
--- The database name will be passed as an argument of the mysql command
+-- The database name will be passed as an argument of the mysql
 SELECT g.`name` AS `genre`,
-COUNT (*) AS `number_of_shows`
-FROM `tv_genre` AS g
-INNER JOIN `tv_show_genres` AS t
-ON g.`id` = t.`genre_id`
+       COUNT(t.`show_id`) AS `number_of_shows`
+FROM `genres` AS g
+INNER JOIN `tv_show_genres` AS t ON g.`id` = t.`genre_id`
 GROUP BY g.`name`
-ORDER BY `number_of_shows` DESC;
+HAVING `number_of_shows` > 0
+ORDER BY `number_of_shows` DESC; 
