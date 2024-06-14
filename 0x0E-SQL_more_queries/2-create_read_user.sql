b6110ec657faf9b4436dd6ba3e3c1633cd0ae065
MizMelly <jazminelike@yahoo.com>
2024-06-14 11:39:53 -0700

    creates database and user

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -4,12 +4,12 @@
 -- If the database hbtn_0d_2 already exists, your script should not fail
 -- If the user user_0d_2 already exists, your script should not fail
 CREATE DATABASE
-IF NOT EXISTS `hbtn_0d_2`; 
+    IF NOT EXISTS `hbtn_0d_2`; 
 CREATE USER
-IF NOT EXISTS 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+    IF NOT EXISTS 'user_0d_2'@'localhost'
+    IDENTIFIED BY 'user_0d_2_pwd';
 GRANT SELECT
-ON `hbtn_0d_2`.*
-TO 'user_0d_2'@'localhost'
-IDENTIFIED BY 'user_0d_2_pwd';
+   ON `hbtn_0d_2`.*
+   TO 'user_0d_2'@'localhost'
+   IDENTIFIED BY 'user_0d_2_pwd';
 FLUSH PRIVILEGES;
