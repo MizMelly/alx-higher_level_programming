3957cd78aa63dda15a604628d77b813a70ba2ae5
MizMelly <jazminelike@yahoo.com>
2024-06-19 14:26:52 -0700

    corrected

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
-#!/usr/bin/env node
-/* a script that prints “JavaScript is amazing” */
+#!/usr/bin/node
+/* a script that prints JavaScript is amazing */
 const myVar = 'JavaScript is amazing';
 console.log(myVar);
