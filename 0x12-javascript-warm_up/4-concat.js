3a7cc1b687938e2cda6a63a9e0834b2f85c1944d
MizMelly <jazminelike@yahoo.com>
2024-06-19 15:39:40 -0700

    javascript warmup

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+/* a script that prints two arguments passed to it, in the following format: “ is ” */
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+
+console.log(arg1 + is + arg2);
