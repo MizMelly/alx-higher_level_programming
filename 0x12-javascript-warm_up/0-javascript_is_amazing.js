fd81c0746b92d8a81d9a9f225d2e161ba77f044b
MizMelly <jazminelike@yahoo.com>
2024-06-19 14:11:13 -0700

    script that prints javascript is amazing

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,4 @@
-#!/bin/usr/node
+#!/usr/bin/env node
 /* a script that prints “JavaScript is amazing” */
-const myVar = “JavaScript is amazing”;
+const myVar = "JavaScript is amazing";
 console.log(myVar);
