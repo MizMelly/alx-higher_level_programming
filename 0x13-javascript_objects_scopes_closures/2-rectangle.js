1103d3824aacaa815e6d4e4cd728d1716cc37c5e
MizMelly <jazminelike@yahoo.com>
2024-06-20 11:56:47 -0700

    updated

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -1,15 +1,12 @@
 #!/usr/bin/node
 /* A class Rectangle that defines a rectangle */
 class Rectangle {
-    constructor(w, h) {
-        if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-            this.width = w;
-            this.height = h;
-        } else {
-           
-            return {};  
-        }
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
     }
+  }
 }
 
 module.exports = Rectangle;
