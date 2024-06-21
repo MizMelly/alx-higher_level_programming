104b24d747d6f634dd0c63bb4705bbfe1fe593ca
MizMelly <jazminelike@yahoo.com>
2024-06-21 02:33:19 -0700

    9-logme.js

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-/*  a function that prints the number of arguments
- already printed and the new argument value */
-exports.logMe = function (item) {
-  if (typeof this.count === 'undefined') {
-    this.count = 0;
-  }
-  console.log(this.count + ' ' + item);
-  this.count++;
-};
+/* a function that prints the number of arguments already printed and the new argument value */
+exports.logMe = (function () {
+  let count = 0;
+  return function (item) {
+    console.log(`${count}: ${item}`);
+    count++;
+  };
+})();
